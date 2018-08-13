9275557d77f5986ddb164091d0e7fb3931c1d3c9
kodemartin <kodemartin@protonmail.ch>
2018-08-13 17:51:37 +0300

    Initialize recipes app

---
+++ recipes/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from farmhash import hash32
+
+# Create your models here.
+class Ingredients(models.Model):
+    TYPES = (
+            ('flour', 'flour'),
+            ('water', 'water'),
+            ('liquid', 'liquid'),
+            ('dry', 'other')
+            )
+    name = models.CharField(max_length=60)
+    variety = models.CharField(max_length=60, null=True, default=None)
+    type = models.CharField(max_length=60, choices=TYPES)
+    hash32 = models.BigIntegerField(null=True)
+
+    def save(self, *args, **kwargs):
+        self.hash32 = hash32(self.name + (self.variety or ''))
+        super().save(*args, **kwargs)
