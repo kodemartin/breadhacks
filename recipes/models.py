fd6cb6ef42feab037096259b2d6f514269846c78
kodemartin <kodemartin@protonmail.ch>
2018-08-17 10:52:06 +0300

    Rename myfields module to customfields

--- recipes/models.py
+++ recipes/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 from farmhash import hash32
-from myfields import UnsignedIntegerField
+from customfields import UnsignedIntegerField
 
 # Create your models here.
 class Ingredient(models.Model):
     TYPES = (
-            ('flour', 'flour'),
-            ('water', 'water'),
-            ('liquid', 'liquid'),
-            ('fat', 'fat'),
-            ('seed', 'seed'),
-            ('other', 'other'),
-            ('meal', 'meal'),
-            ('malt', 'malt')
-            )
+        ('flour', 'flour'),
+        ('water', 'water'),
+        ('liquid', 'liquid'),
+        ('fat', 'fat'),
+        ('seed', 'seed'),
+        ('other', 'other'),
+        ('meal', 'meal'),
+        ('malt', 'malt')
+        )
     name = models.CharField(max_length=60)
     variety = models.CharField(max_length=60, null=True, default=None)
     type = models.CharField(max_length=60, choices=TYPES)
