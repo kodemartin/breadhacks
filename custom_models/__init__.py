62f0ac7befb2fd9aa2fd55b406045c06f90ff6e6
kodemartin <kodemartin@protonmail.ch>
2018-09-23 18:33:33 +0300

    Create abstract class for hashable (32bit) models

---
+++ custom_models/__init__.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from customfields import UnsignedIntegerField
+
+
+class Hash32Model(models.Model):
+
+    hash32 = UnsignedIntegerField(unique=True, null=True)
+
+    class Meta:
+        abstract = True
+
+    def evaluate_hash(self):
+        NotImplemented
+
+    def update_hash(self):
+        self.hash32 = self.evaluate_hash()
