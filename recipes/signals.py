88a93dac46e45c4dc942bc8c09f5ef3b210a6b5a
kodemartin <kodemartin@protonmail.ch>
2018-10-13 13:37:07 +0300

    Disable signal 'update_mixture_hash'

--- recipes/signals.py
+++ recipes/signals.py
@@ -3,14 +3,9 @@ import farmhash
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 
-from .models import Ingredient, MixtureIngredients
+from .models import Ingredient
 
 
 @receiver(pre_save, sender=Ingredient)
 def evaluate_hash(sender, instance, **kwargs):
     instance.update_hash()
-
-@receiver(post_save, sender=MixtureIngredients)
-def update_mixture_hash(sender, instance, **kwargs):
-    instance.mixture.update_properties()
-    instance.mixture.save()
