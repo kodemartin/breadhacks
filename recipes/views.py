259e7666f554d6809ff0676593fcf4b2c9037056
Konstantinos Demartinos <kodemartin@protonmail.ch>
2019-07-24 18:39:19 +0300

    Add view of ingredients
    * Add new url in recipes.url
    * Add new view in recipes.views
    * Add recipes.templates
    * Add recipes.static
    * Update project settings to include recipes urls

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Ingredient
 
 # Create your views here.
+def ingredients(request):
+    ingredients = Ingredient.objects.order_by('type')
+    ingredients_per_type = {}
+    for i in ingredients:
+        try:
+            ingredients_per_type[i.type].append(i.name)
+        except KeyError:
+            ingredients_per_type[i.type] = [i.name,]
+    template = loader.get_template('ingredients/list.html')
+    context = {'ingredients': ingredients_per_type,
+               'header': 'Available ingredients'}
+    return HttpResponse(template.render(context, request))
