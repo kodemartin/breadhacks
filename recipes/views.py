c5f9846a973bb35984f733cbd294e530be8bc38e
Konstantinos Demartinos <kodemartin@protonmail.ch>
2019-07-24 18:46:14 +0300

    Revise ingredients view

--- recipes/views.py
+++ recipes/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 
 from .models import Ingredient
 
 # Create your views here.
 def ingredients(request):
     ingredients = Ingredient.objects.order_by('type')
+
+    # Classify the ingredients according to type
     ingredients_per_type = {}
     for i in ingredients:
+        display_name = ' '.join(filter(None, (i.name, i.variety)))
         try:
-            ingredients_per_type[i.type].append(i.name)
+            ingredients_per_type[i.type].append(display_name)
         except KeyError:
-            ingredients_per_type[i.type] = [i.name,]
-    template = loader.get_template('ingredients/list.html')
+            ingredients_per_type[i.type] = [display_name,]
+
     context = {'ingredients': ingredients_per_type,
                'header': 'Available ingredients'}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'ingredients/list.html', context)
