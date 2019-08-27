d53b085509242a8ef03f1e9cf995966fbff2f3d6
Konstantinos Demartinos <kodemartin@protonmail.ch>
2019-08-27 16:49:15 +0300

    Register Ingredient in admin site

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Ingredient
+from .models import Ingredient, Mixture, MixtureIngredients
 
 admin.site.register(Ingredient)
