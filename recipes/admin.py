545ee30b3328b790eba573a716ac7780dd50bf0a
Konstantinos Demartinos <kodemartin@protonmail.ch>
2019-09-13 14:17:59 +0300

    Clean up recipes/admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Ingredient, Mixture, MixtureIngredients
+from .models import Ingredient
 
 admin.site.register(Ingredient)
