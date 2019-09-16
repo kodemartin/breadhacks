d9ef1677531ee2ca436e826f770970b63ae30910
Konstantinos Demartinos <kodemartin@protonmail.ch>
2019-09-16 17:45:30 +0300

    Add recipes/new endpoint

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     re_path(r'^ingredients/(?:$|index/$)', views.ingredients,
             name='ingredients-index'),
-    path('mixture/new/', views.new_mixture, name='mixture-new')
+    path('mixture/new/', views.new_mixture, name='mixture-new'),
+    path('mixture/preview', views.mixture_preview, name='mixture-preview'),
+    path('new/', views.new_recipe, name='recipe-new'),
 ]
