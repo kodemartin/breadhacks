e6d192c7cb9378461b402308dcd804a64db777a7
Konstantinos Demartinos <kodemartin@protonmail.ch>
2019-09-27 18:00:08 +0300

    Add new endpoints
    * mixture/list/ingredients
    * mixture/load/

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,8 +6,11 @@ urlpatterns = [
     re_path(r'^ingredients/(?:$|index/$)', views.list_ingredients,
             name='ingredients-index'),
     path('mixture/new/', views.add_new_mixture, name='mixture-new'),
+    path('mixture/load/', views.load_mixture, name='mixture-load'),
     path('mixture/preview', views.mixture_preview, name='mixture-preview'),
     path('mixture/list/partial/', views.list_partial_mixtures,
          name='mixture-list-partial'),
+    path('mixture/list/ingredients', views.list_mixture_ingredients,
+         name='mixture-list-ingredients'),
     path('new/', views.RecipeFormView.as_view(), name='recipe-new'),
 ]
